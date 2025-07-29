45fdad7a7a6d4961e565797cb5e829bf7d1972b9
Nourhan Adel <91224846+NorhanAdel@users.noreply.github.com>
2025-07-29 12:45:30 +0300

    Delete server directory

--- server/routes/google.js
+++
@@ -1,8 +0,0 @@
- 
-const express = require("express");
-const router = express.Router();
-const { googleLogin } = require("../controller/google");
-
-router.post("/google-login", googleLogin);
-
-module.exports = router;
