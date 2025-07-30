a603613f29de8d0ffbfbf7cdd8300cc938a77558
Nourhan Adel <91224846+NorhanAdel@users.noreply.github.com>
2025-07-30 12:11:11 +0300

    Update Login.jsx

--- frontend/src/Pages/Auth/Login.jsx
+++ frontend/src/Pages/Auth/Login.jsx
@@ -13,7 +13,7 @@ function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/auth/login", { email, password });
+      const res = await axios.post("http://localhost:5000/api/auth/login", { email, password });
 
       login(res.data.token);
       navigate("/");
